607cff60dc3f34194f9115962652aa3cf2671f43
Panagiotis Evangelopoulos <evangelopoulos.panagiotis@gmail.com>
2016-10-05 21:35:20 +0300

    Initial Commit!

---
+++ Assets/play_sound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class play_sound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+   
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.PlayDelayed(1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
